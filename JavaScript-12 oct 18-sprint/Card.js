ee8a3b81c132549990b92e3e46e894df89f230a4
michel azzam <michel.azzam@lau.edu>
2018-10-12 12:34:58 +0300

    12 oct 18

--- JavaScript-12 oct 18-sprint/Card.js
+++ JavaScript-12 oct 18-sprint/Card.js
@@ -3,12 +3,13 @@ class Card{
     constructor (src){
         this.src=src;
         this.front="yellow.png";
+        
         this.imgHtml=document.createElement('img');
         this.imgHtml.src=this.front;
 
-        this.imgHtml.addEventListener("click", this.switchCard());
-        // this.switchCard.bind(this);
-    this.switchCard=this.switchCard.bind(this);
+        this.switchCard=this.switchCard.bind(this);
+        this.imgHtml.addEventListener("click", this.switchCard);
+
     }
 
 
--- JavaScript-12 oct 18-sprint/images.js
+++ JavaScript-12 oct 18-sprint/images.js
@@ -1,4 +1,4 @@
-var images=["image1.JPEG","image2.JPEG","image3.JPEG","image4.JPEG","image5.JPEG"];
+var images=["image1.JPG","image2.JPG","image3.JPG","image4.JPG","image5.JPG"];
 
 var card=[],index,cardOriginal=[];
 var l=images.length;
@@ -6,24 +6,27 @@ var l=images.length;
 
 for(let i=0;i<l;i++){
   card.push(new Card(images[i]));
+  
 }
 
 
 
-while(l>0)
+while(l>=0)
 {
     let temp;
-    index = Math.floor((Math.random() * 10) + 1);
-    if(index<6){
+    index = Math.floor((Math.random() * 10) );
+    if(index<5){
         temp=images[images.length-1];
         images[images.length-1]=images[index];
         images[index]=temp;
         l--;
     }
 }
+
 //now images are shuffled
 for(let i=0;i<images.length;i++){
     cardOriginal.push(new Card(images[i]));
+    
   }
 
 const c=document.querySelector('.constructor');
